ec8f5d714bb2286653412ac8018235f43e8707d4
hgreen37 <hgreen37@gatech.edu>
2023-06-27 18:03:43 -0400

    First Check

---
+++ init.py
@@ -0,0 +1,14 @@
+import finnhub
+
+# Setup client
+finnhub_client = finnhub.Client(api_key="ciafar1r01qn522c0hc0ciafar1r01qn522c0hcg")
+
+# Stock candles
+res = finnhub_client.stock_candles('AAPL', 'D', 1590988249, 1591852249)
+print(res)
+
+#Convert to Pandas Dataframe
+import pandas as pd
+print(pd.DataFrame(res))
+
+
