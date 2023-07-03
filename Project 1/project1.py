385e89048303553be8f1250cef5648c993f46d41
hgreen37 <hgreen37@gatech.edu>
2023-07-03 13:25:36 -0400

    proj 1

--- Project 1/project1.py
+++ Project 1/project1.py
@@ -8,6 +8,7 @@ import pandas as pd  # to handle tabular data well
 import requests
 import xlsxwriter
 import math
+import time
 
 
 #api call to client
@@ -16,10 +17,54 @@ finnhub_client = finnhub.Client(api_key="ciafar1r01qn522c0hc0ciafar1r01qn522c0hc
 
 
 #Importing th elist of stocks
+#Change for your path
+stocks = pd.read_csv('/Users/harrygraneygreen/Desktop/tradingBot/tradingBotApp/Project 1/sp_500_stocks.csv')
+#print(stocks)
 
-stocks = pd.read_csv('sp_500_stocks.csv')
+symbol = 'AAPL'
 
-print(stocks)
+#we need market captilization for each stcok
+#also price of each stock
+quote = finnhub_client.quote('AAPL')
+price = quote.get('c')
+#c is for current stock price
+
+
+#Build pandas data frame : ticker, price, # of shares to buy
+my_columns = [ 'Ticker', 'Stock Price', 'Number of Shares to Buy']
+
+#final_dataframe = pd.DataFrame(columns = my_columns)
+
+#adding one row to our Data Frame
+
+#final_dataframe.loc[len(final_dataframe.index)] = [symbol,price,'N/A']
+
+#now loop through every ticker in stocks
+
+
+final_dataframe = pd.DataFrame(columns = my_columns)
+
+for stock in stocks['Ticker']:
+    time.sleep(1)
+    #symbol = stock
+    price = quote = finnhub_client.quote(stock)
+    price = quote.get('c')
+    final_dataframe.loc[len(final_dataframe.index)] = [stock,price,'N/A']
+
+
+print(final_dataframe)
+    
+
+
+
+
+
+
+
+
+
+
+
 
 
 
